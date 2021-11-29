569252721d41800b9ae2be3f1bfdb6e92e1fb6c8
OsvaldoMDLM <osvaldo.benjamin31@gmail.com>
2021-11-29 17:35:28 -0600

    commit

---
+++ src/components/ItemList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Image, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+const ItemList = () => {
+  return (
+    <View>
+      <Divider></Divider>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default ItemList;
